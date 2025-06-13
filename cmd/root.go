8460f5e0a5da7b7e6f59ab5d9747abaf17191579
a9sk <920a9sk42f76c765@proton.me>
2025-06-14 00:00:37 +0200

    feat: migrate width and height to terminal size detection

--- cmd/root.go
+++ cmd/root.go
@@ -1,22 +1,28 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/a9sk/polarrose/internal/render"
 	"github.com/a9sk/polarrose/internal/rose"
+	"github.com/a9sk/polarrose/internal/terminal"
 )
 
-// TODO: migrate width and height to terminal size detection
 const (
-	width  = 40
-	height = 20
-	steps  = 100000
-	char   = ':' // temprorary character for drawing
+	steps = 100000
+	char  = ':' // temprorary character for drawing
 )
 
 func Root(size float64, petals int, color string) {
 	// TODO: validate input, comes from main for now so no need to do that (validated there)
 
 	points := rose.GenerateRosePoints(size, petals, steps)
+
+	width, height, err := terminal.GetRoseSize()
+	if err != nil {
+		fmt.Printf(fmt.Sprintf("[FAIL] in TERMINAL: %v", err))
+		return
+	}
 
 	render.DrawASCII(points, width, height, char, size, color)
 }
