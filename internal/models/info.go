e68a70f20346dddf5bf32c7fd1e6ebc82bd50a44
a9sk <920a9sk42f76c765@proton.me>
2025-06-14 21:57:03 +0200

    feat: struct for system infos

---
+++ internal/models/info.go
@@ -0,0 +1,14 @@
+package models
+
+type Info struct {
+	OS       string
+	Arch     string
+	Kernel   string
+	Version  string
+	Uptime   string
+	Hostname string
+	Platform string
+	CPU      string
+	GPU      string
+	Memory   string
+}
