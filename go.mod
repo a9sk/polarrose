77c30ce12c5d911e4a7adf43986cb5b9321df76c
a9sk <920a9sk42f76c765@proton.me>
2025-06-16 22:26:41 +0200

    feat: sysinfo package to gather system information

--- go.mod
+++ go.mod
@@ -5,6 +5,18 @@ go 1.23.0
 // i have no idea of how this works, i just run go mod tidy and pray
 toolchain go1.23.10
 
-require golang.org/x/term v0.32.0
+require (
+	github.com/shirou/gopsutil/v4 v4.25.5
+	golang.org/x/term v0.32.0
+)
 
-require golang.org/x/sys v0.33.0 // indirect
+require (
+	github.com/ebitengine/purego v0.8.4 // indirect
+	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
+	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
+	github.com/tklauser/go-sysconf v0.3.12 // indirect
+	github.com/tklauser/numcpus v0.6.1 // indirect
+	github.com/yusufpapurcu/wmi v1.2.4 // indirect
+	golang.org/x/sys v0.33.0 // indirect
+)
---
+++ internal/sysinfo/infos.go
@@ -0,0 +1,66 @@
+package sysinfo
+
+import (
+	"fmt"
+
+	"github.com/a9sk/polarrose/internal/models"
+	"github.com/shirou/gopsutil/v4/host"
+)
+
+// the structs below are used to gather system information
+// REFERENCE: https://pkg.go.dev/github.com/shirou/gopsutil/v4
+//
+//
+// type InfoStat struct {
+// 	Hostname             string `json:"hostname"`
+// 	Uptime               uint64 `json:"uptime"`
+// 	BootTime             uint64 `json:"bootTime"`
+// 	Procs                uint64 `json:"procs"`           // number of processes
+// 	OS                   string `json:"os"`              // ex: freebsd, linux
+// 	Platform             string `json:"platform"`        // ex: ubuntu, linuxmint
+// 	PlatformFamily       string `json:"platformFamily"`  // ex: debian, rhel
+// 	PlatformVersion      string `json:"platformVersion"` // version of the complete OS
+// 	KernelVersion        string `json:"kernelVersion"`   // version of the OS kernel (if available)
+// 	KernelArch           string `json:"kernelArch"`      // native cpu architecture queried at runtime, as returned by `uname -m` or empty string in case of error
+// 	VirtualizationSystem string `json:"virtualizationSystem"`
+// 	VirtualizationRole   string `json:"virtualizationRole"` // guest or host
+// 	HostID               string `json:"hostId"`             // ex: uuid
+// }
+
+func GetSysInfo() (*models.Info, error) {
+	// i don't think this is how you do it
+	err := error(nil)
+
+	// we should gather system information here and return a models.Info struct
+	info := &models.Info{}
+
+	if err = getHostInfo(info); err != nil {
+		return nil, fmt.Errorf("failed to get host info: %w", err)
+	}
+
+	return info, nil
+}
+
+func getHostInfo(info *models.Info) error {
+
+	hostInfo, err := host.Info()
+	if err != nil {
+		return err
+	}
+
+	info.Hostname = hostInfo.Hostname
+
+	info.Uptime = fmt.Sprintf("%d", hostInfo.Uptime) // parse to string
+
+	info.OS = hostInfo.OS
+
+	info.Platform = hostInfo.Platform
+
+	info.Kernel = hostInfo.KernelVersion
+
+	info.Arch = hostInfo.KernelArch
+
+	info.Version = hostInfo.PlatformVersion
+
+	return nil
+}
