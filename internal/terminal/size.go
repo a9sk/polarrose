d718de668e8f536019935d35491ea91f3fe2813b
a9sk <920a9sk42f76c765@proton.me>
2025-06-17 19:41:08 +0200

    feat: set terminal to raw mode

--- internal/terminal/size.go
+++ internal/terminal/size.go
@@ -5,12 +5,16 @@ package terminal
 
 import (
 	"fmt"
+	"os"
 
 	"golang.org/x/term"
 )
 
 func GetFullSize() (int, int, error) {
+
 	// TODO: check why powershell does not work
+	setTerminalRaw()
+
 	w, h, err := term.GetSize(0) // standard input (stdin) sizes
 	if err != nil {
 		// maybe a panic might be good here, similar to GetRoseSize
@@ -35,4 +39,17 @@ func GetRoseSize() (int, int, error) {
 	// the width is half the terminal to leave space for sysinfo
 	// the height is 85% of the terminal so that it fits nicely
 	return w / 2, h - (h * 15 / 100), nil
+}
+
+func setTerminalRaw() {
+
+	// this is a workaround to make sure that the terminal is in raw mode
+	// it is needed for the term.GetSize to work properly
+	// it is not needed for the terminal size detection itself
+
+	oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
+	if err != nil {
+		panic(err)
+	}
+	defer term.Restore(int(os.Stdin.Fd()), oldState)
 }
