a5fc0c107d6effa1d890774b11967e589cc83242
a9sk <920a9sk42f76c765@proton.me>
2025-08-21 23:29:06 +0200

    feat: reorder point generation and rendering

--- cmd/root.go
+++ cmd/root.go
@@ -17,19 +17,24 @@ const (
 func Root(size float64, petals int, color string) {
 	// TODO: validate input, comes from main for now so no need to do that (validated there)
 
-	points := rose.GenerateRosePoints(size, petals, steps)
-
 	width, height, err := terminal.GetRoseSize()
 	if err != nil {
 		panic(fmt.Sprintf("[FAIL] in TERMINAL: %v", err))
 	}
 
+	// show the external points
+	points := rose.GenerateRosePoints(size, petals, steps)
 	render.DrawASCII(points, width, height, char, size, color)
+
+	// show the internal points
+	pointsIn := rose.GetInternalPoints(points)
+	render.DrawASCII(pointsIn, width, height, char, size, color)
 
 	infos, err := sysinfo.GetSysInfo()
 	if err != nil {
 		panic(fmt.Sprintf("[FAIL] in SYSINFO: %v", err))
 	}
 
+	// show the system information
 	render.DrawInfo(infos)
 }
