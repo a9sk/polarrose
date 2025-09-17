92161daba38477d8a32386df6fe263ab3c3fabb7
a9sk <920a9sk42f76c765@proton.me>
2025-09-17 14:49:13 +0200

    fix: enhanced printing final spaces

--- internal/models/credits.go
+++ internal/models/credits.go
@@ -1,6 +1,6 @@
 package models
 
-var Watermark = []string{"a9sk  (Emiliano Rizzonelli)", "github.com/a9sk/polarrose"}
+var Watermark = []string{"a9sk  (Emiliano Rizzonelli)", "github.com/a9sk/polarrose  "}
 
 func GetWatermark() string {
 	return Watermark[0] + " source: " + Watermark[1]
